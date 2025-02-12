a24d9e84198048c3300ee3032f74cefad7f98fe7
Mateus Prado <mateus.prado@me.com>
2025-02-12 22:17:56 +0000

    feat (home): under construction page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <h1>Home</h1>
+  return (
+    <h1>
+      This is under construction project. We're building a nice space in the
+      internet. Our commitment will be to provide be best place for you to find
+      quality content about ADHD.
+    </h1>
+  )
 }
