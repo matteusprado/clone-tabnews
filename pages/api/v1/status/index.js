1867e498474c6c26d24741d3a079a74936327b51
Mateus Prado <mateus.prado@me.com>
2025-02-13 22:11:28 -0500

    feat (status): display database status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,26 @@
 import database from 'infra/database.js'
 
 async function status(req, res) {
-  const result = await database.query('SELECT 1 + 1 as sum')
-  console.log(result.rows)
+  const updatedAt = new Date().toISOString()
+  const databaseVersionResult = await database.query(`show server_version;`)
+  const version = databaseVersionResult.rows[0].server_version
+  const maxConnectionsResult = await database.query('SHOW max_connections;')
+  const maxConnections = parseInt(maxConnectionsResult.rows[0].max_connections)
+  const databaseName = process.env.POSTGRES_DB
+  const openedConnectionsResult = await database.query({
+    text: 'SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;',
+    values: [databaseName]
+  })
+  const openedConnections = openedConnectionsResult.rows[0].count
   res.status(200).json({
-    key: 'value',
+    updated_at: updatedAt, 
+    dependencies:{
+      database: {
+        version, 
+        max_connections: maxConnections,
+        opened_connections: openedConnections
+      }
+    }
   })
 }
 
