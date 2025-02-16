c143c3c2cf1cd63388480ca8cfecc13f283c0e94
Mateus Prado <mateus.prado@me.com>
2025-02-16 12:05:30 -0500

    chore (migrations): add migration tests

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,16 @@
+import migrationRunner from 'node-pg-migrate'
+import { join } from 'node:path'
+
+export default async function migrations(req, res) {
+  const migrations = await migrationRunner({
+    databaseUrl: process.env.DATABASE_URL,
+    dryRun: true,
+    dir: join("infra", "migrations"), 
+    direction: 'up',
+    migrationsTable: 'pgmigrations',
+    verbose: true
+  })
+  res.status(200).json(migrations)
+}
+
+
